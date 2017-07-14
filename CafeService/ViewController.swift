17ba2b07aa94358a140bde983b6cc630d03b8894
ZYFAR <zyfar@ITLabAdmins-iMac-2.local>
2017-07-14 14:09:40 +0600

    Create Project

---
+++ CafeService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CafeService
+//
+//  Created by ZYFAR on 14.07.17.
+//  Copyright © 2017 iCoder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
